4f8ce7bd3398778a1c81703be8886fde28f3b112
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-01 21:50:11 +0200

    refactor and clean up

--- lib/src/features/main_screen.dart
+++ lib/src/features/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/string_extensions.dart';
+import 'app_tab.dart';
 import 'about/about_page.dart';
 import 'current/current_weather_page.dart';
 import 'forecast/forecast_weather_page.dart';
@@ -18,59 +20,35 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
+  int _selectedTab = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Weather App"),
-        backgroundColor: Colors.deepPurple,
       ),
       body: IndexedStack(
-        index: _selectedIndex,
+        index: _selectedTab,
         children: _pages,
       ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: _onTabTapped,
-        currentIndex: _selectedIndex,
-        // type: BottomNavigationBarType.shifting,
-        backgroundColor: Colors.deepPurple,
-        iconSize: 40.0,
-        selectedFontSize: 20,
-        // unselectedFontSize: 20,
-        // showSelectedLabels: false,
-        // showUnselectedLabels: false,
-        selectedIconTheme: const IconThemeData(
-          color: Colors.amberAccent,
-          // size: 40,
-        ),
-        selectedItemColor: Colors.amberAccent,
-        // selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
-        unselectedIconTheme: const IconThemeData(
-          color: Colors.red,
-          // size: 40,
-        ),
-        unselectedItemColor: Colors.red,
-        // unselectedLabelStyle: ,
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.wb_sunny_rounded),
-              label: "Current",
-              tooltip: "Current"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bar_chart_rounded),
-            label: "Forecast",
-            tooltip: "Forecast",
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.info_rounded), label: "About", tooltip: "About"),
-        ],
+        currentIndex: _selectedTab,
+        items: AppTab.values
+            .map(
+              (tab) => BottomNavigationBarItem(
+                icon: Icon(tab.icon),
+                label: tab.name.capitalized,
+                tooltip: tab.tooltip,
+              ),
+            )
+            .toList(),
       ),
     );
   }
 
   void _onTabTapped(int index) {
-    setState(() => _selectedIndex = index);
+    setState(() => _selectedTab = index);
   }
 }
