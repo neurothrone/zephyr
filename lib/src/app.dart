9d8000d516395f155898d81e57fc15cf9a429f14
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-01 21:49:35 +0200

    configure app theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'core/palette.dart';
 import 'features/main_screen.dart';
 
 class MainApp extends StatelessWidget {
@@ -7,10 +8,27 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Weather App",
-      home: MainScreen(),
+      home: const MainScreen(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Palette.darkBlue,
+        ),
+        scaffoldBackgroundColor: Palette.darkBlue.withOpacity(0.5),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Palette.darkBlue,
+          selectedItemColor: Palette.lightOrange,
+          selectedIconTheme: IconThemeData(color: Palette.lightOrange),
+          selectedLabelStyle: TextStyle(fontSize: 20.0),
+          unselectedItemColor: Palette.darkOrange,
+          unselectedIconTheme: IconThemeData(color: Palette.darkOrange),
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
