e27e26700980b19650c8889e54c1a8ce2a69885f
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-01 20:58:55 +0200

    add placeholder pages

---
+++ lib/src/features/current/current_weather_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CurrentWeatherPage extends StatelessWidget {
+  const CurrentWeatherPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Current Weather Page"),
+    );
+  }
+}
