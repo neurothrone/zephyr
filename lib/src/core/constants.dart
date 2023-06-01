46492e871f336cd6d224fc4861c7f52bc37d6f7f
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-01 21:22:11 +0200

    move colors to custom class

--- lib/src/core/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/rendering.dart';
-
-// Color palette from:
-// https://coolors.co/palette/8ecae6-219ebc-023047-ffb703-fb8500
-const lightBlue = Color(0xFF8ECAE6);
-const mediumBlue = Color(0xFF219EBC);
-const darkBlue = Color(0xFF023047);
-const lightOrange = Color(0xFFFFB703);
-const darkOrange = Color(0xFFFB8500);
---
+++ lib/src/core/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/rendering.dart';
+
+// Color palette from:
+// https://coolors.co/palette/8ecae6-219ebc-023047-ffb703-fb8500
+class Palette {
+  static const lightBlue = Color(0xFF8ECAE6);
+  static const mediumBlue = Color(0xFF219EBC);
+  static const darkBlue = Color(0xFF023047);
+  static const lightOrange = Color(0xFFFFB703);
+  static const darkOrange = Color(0xFFFB8500);
+}
