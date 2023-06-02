8440c7a80255578080037bee936c9276ff9a4caf
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-02 19:14:37 +0200

    add statement to load environment file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'src/app.dart';
 
-void main() {
+Future<void> main() async {
+  // Attempt to load in environment variables from .env file
+  await dotenv.load(fileName: ".env");
+
   runApp(const MainApp());
 }
