54365de48668c4bac7c20fda74b756145125d8ee
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-03 00:43:58 +0200

    customize buttons and tab styling

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -19,14 +19,48 @@ class MainApp extends StatelessWidget {
         scaffoldBackgroundColor: Palette.darkBlue.withOpacity(0.5),
         bottomNavigationBarTheme: const BottomNavigationBarThemeData(
           backgroundColor: Palette.darkBlue,
-          selectedItemColor: Palette.lightOrange,
-          selectedIconTheme: IconThemeData(color: Palette.lightOrange),
+          selectedItemColor: Palette.darkOrange,
+          selectedIconTheme: IconThemeData(color: Palette.darkOrange),
           selectedLabelStyle: TextStyle(fontSize: 20.0),
-          unselectedItemColor: Palette.darkOrange,
-          unselectedIconTheme: IconThemeData(color: Palette.darkOrange),
+          unselectedItemColor: Palette.lightOrange,
+          unselectedIconTheme: IconThemeData(color: Palette.lightOrange),
         ),
         textTheme: const TextTheme(
           bodyMedium: TextStyle(color: Colors.white),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith(
+              (Set<MaterialState> states) {
+                if (states.contains(MaterialState.disabled)) {
+                  return Colors.grey;
+                }
+                return Palette.darkOrange;
+              },
+            ),
+            foregroundColor: MaterialStateProperty.all(Colors.white),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+            ),
+          ),
+        ),
+        segmentedButtonTheme: SegmentedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith<Color>(
+              (Set<MaterialState> states) {
+                if (states.contains(MaterialState.disabled)) {
+                  return Colors.grey;
+                } else if (states.contains(MaterialState.selected)) {
+                  return Palette.darkOrange;
+                }
+                return Palette.darkOrange.withOpacity(0.5);
+              },
+            ),
+            foregroundColor: MaterialStateProperty.all(Colors.white),
+            side: MaterialStateProperty.all(
+              const BorderSide(color: Colors.black),
+            ),
+          ),
         ),
       ),
     );
