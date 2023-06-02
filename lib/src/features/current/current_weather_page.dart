97f62ad1468156ea6a08a9ad9e18dec30c898db8
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-03 00:44:26 +0200

    add segmented button and improve ui

--- lib/src/features/current/current_weather_page.dart
+++ lib/src/features/current/current_weather_page.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 import '../../common_widgets/custom_circular_progress_indicator.dart';
 import '../../core/constants.dart';
+import '../../core/palette.dart';
 import '../../services/weather.dart';
 import '../../services/weather_service.dart';
 import 'current_weather_display.dart';
+
+enum WeatherLocationType {
+  current,
+  custom,
+}
 
 class CurrentWeatherPage extends StatefulWidget {
   const CurrentWeatherPage({Key? key}) : super(key: key);
@@ -18,40 +24,56 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
   bool _isLoading = false;
 
   Weather? _currentWeather;
+  WeatherLocationType _locationType = WeatherLocationType.current;
 
   @override
   Widget build(BuildContext context) {
-    return _isLoading
-        ? const CustomCircularProgressIndicator()
-        : SingleChildScrollView(
-            padding: const EdgeInsets.all(kPadding20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                ElevatedButton(
-                  onPressed: _getCurrentWeather,
-                  child: const Text("Get Current Weather"),
-                ),
-                const SizedBox(height: kPadding20),
-                _currentWeather != null
-                    ? CurrentWeatherDisplay(weather: _currentWeather!)
-                    : const Text(
-                        "No data yet",
-                        textAlign: TextAlign.center,
-                      ),
-              ],
-            ),
-          );
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(kPadding20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SegmentedButton<WeatherLocationType>(
+            segments: const [
+              ButtonSegment(
+                value: WeatherLocationType.current,
+                label: Text("Current"),
+                icon: Icon(Icons.home_rounded),
+              ),
+              ButtonSegment(
+                value: WeatherLocationType.custom,
+                label: Text("Custom"),
+                icon: Icon(Icons.pin_drop_rounded),
+              ),
+            ],
+            selected: <WeatherLocationType>{_locationType},
+            onSelectionChanged: _isLoading
+                ? null
+                : (Set<WeatherLocationType> newSelection) {
+                    setState(() => _locationType = newSelection.first);
+                  },
+          ),
+          const SizedBox(height: kPadding20),
+          ElevatedButton(
+            onPressed: _isLoading ? null : _getCurrentWeather,
+            child: const Text("Get Current Weather"),
+          ),
+          const SizedBox(height: kPadding40),
+          _isLoading
+              ? const CustomCircularProgressIndicator()
+              : _currentWeather != null
+                  ? CurrentWeatherDisplay(weather: _currentWeather!)
+                  : const SizedBox.shrink()
+        ],
+      ),
+    );
   }
 
-  // 55.61234260391604, 12.980266915343263
   Future<void> _getCurrentWeather() async {
     _currentWeather = null;
 
     setState(() => _isLoading = true);
 
-    // await Future.delayed(const Duration(seconds: 2));
     final weather = await _weatherService.getCurrentWeather(
       55.61234260391604,
       12.980266915343263,
