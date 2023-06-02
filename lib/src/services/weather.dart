18309059761b34eb034253153423cb5379b8c91d
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-02 19:13:32 +0200

    create a weather model

---
+++ lib/src/services/weather.dart
@@ -0,0 +1,29 @@
+class Weather {
+  Weather({
+    required this.latitude,
+    required this.longitude,
+    required this.description,
+    required this.temperature,
+    required this.city,
+    required this.country,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      latitude: json["coord"]["lat"],
+      longitude: json["coord"]["lon"],
+      description: json["weather"][0]["description"],
+      temperature: json["main"]["temp"],
+      city: json["name"],
+      country: json["sys"]["country"],
+    );
+  }
+
+  final double latitude;
+  final double longitude;
+  final String description;
+  final double temperature;
+  final String city;
+  final String country;
+  final DateTime createdAt = DateTime.now();
+}
