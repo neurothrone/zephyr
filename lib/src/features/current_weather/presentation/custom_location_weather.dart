88c67a679eb7edab167e997c3d07b99a189dd57c
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-03 05:54:13 +0200

    integrate geolocator

--- lib/src/features/current_weather/presentation/custom_location_weather.dart
+++ lib/src/features/current_weather/presentation/custom_location_weather.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import '../../../common_widgets/custom_circular_progress_indicator.dart';
+import '../../../core/constants.dart';
+import '../../../services/weather_service.dart';
+import '../data/location_weather.dart';
+import 'weather_display.dart';
+
 class CustomLocationWeather extends StatefulWidget {
   const CustomLocationWeather({Key? key}) : super(key: key);
 
@@ -8,10 +17,63 @@ class CustomLocationWeather extends StatefulWidget {
 }
 
 class _CustomLocationWeatherState extends State<CustomLocationWeather> {
+  final _weatherService = WeatherService();
+  LocationWeather? _myLocationWeather;
+  bool _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Custom Location Weather"),
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: _isLoading ? null : _getCurrentWeather,
+          child: const Text("Get Custom Weather"),
+        ),
+        const SizedBox(height: kPadding20),
+        if (_isLoading) ...[
+          const CustomCircularProgressIndicator()
+        ] else ...[
+          if (_myLocationWeather != null)
+            WeatherDisplay(weather: _myLocationWeather!)
+        ],
+      ],
     );
+  }
+
+  Future<Position?> _getCurrentLocation() async {
+    PermissionStatus permissionStatus = await Permission.location.request();
+
+    if (permissionStatus.isGranted) {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      return position;
+    } else {
+      return null;
+    }
+  }
+
+  Future<void> _getCurrentWeather() async {
+    _myLocationWeather = null;
+
+    setState(() => _isLoading = true);
+
+    final position = await _getCurrentLocation();
+
+    if (position == null) {
+      setState(() => _isLoading = false);
+      return;
+    }
+
+    final weather = await _weatherService.getCurrentWeather(
+      position.latitude,
+      position.longitude,
+    );
+
+    if (weather != null) {
+      setState(() => _myLocationWeather = weather);
+    }
+
+    setState(() => _isLoading = false);
   }
 }
