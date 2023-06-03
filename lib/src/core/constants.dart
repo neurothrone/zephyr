1cfb88050ca0db698d7066fd59cb0620eda07828
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-03 07:00:40 +0200

    add constants

--- lib/src/core/constants.dart
+++ lib/src/core/constants.dart
@@ -1,4 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'palette.dart';
+
 const kPadding0 = 0.0;
 const kPadding10 = 10.0;
 const kPadding20 = 20.0;
 const kPadding40 = 40.0;
+
+const kBorderRadius = BorderRadius.all(
+  Radius.circular(20.0),
+);
+
+const kOutlineInputBorder = OutlineInputBorder(
+  borderSide: BorderSide(color: Palette.darkOrange),
+  borderRadius: kBorderRadius,
+);
+
+const kCustomBottomNavigationBarThemeData = BottomNavigationBarThemeData(
+  backgroundColor: Palette.darkBlue,
+  selectedItemColor: Palette.darkOrange,
+  selectedIconTheme: IconThemeData(color: Palette.darkOrange),
+  selectedLabelStyle: TextStyle(fontSize: 20.0),
+  unselectedItemColor: Palette.lightOrange,
+  unselectedIconTheme: IconThemeData(color: Palette.lightOrange),
+);
