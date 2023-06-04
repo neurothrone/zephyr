9b67c6e9e1474ee62837d9b3c9aea8fa650ec129
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-04 05:24:55 +0200

    create location service

---
+++ lib/src/services/location_service.dart
@@ -0,0 +1,17 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class LocationService {
+  Future<Position?> getCurrentLocation() async {
+    PermissionStatus permissionStatus = await Permission.location.request();
+
+    if (permissionStatus.isGranted) {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      return position;
+    } else {
+      return null;
+    }
+  }
+}
