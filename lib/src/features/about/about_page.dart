46a4e3ee428c58d490ce449874c7bc0985e5874a
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-04 04:34:13 +0200

    refactor about page

--- lib/src/features/about/about_page.dart
+++ lib/src/features/about/about_page.dart
@@ -1,42 +1,17 @@
 import 'package:flutter/material.dart';
 
-import '../../core/constants.dart';
+import 'about_content.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(kPadding20),
-      child: SizedBox(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              "Project Weather",
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: kPadding20),
-            SizedBox(
-              width: mediaQuery.orientation == Orientation.portrait
-                  ? mediaQuery.size.width
-                  : mediaQuery.size.width / 2,
-              child: const Text(
-                "This is an app that is developed for the course 1DV535 at "
-                "Linneaus University using Flutter and the OpenWeatherMap API.",
-              ),
-            ),
-            const SizedBox(height: kPadding10),
-            const Text("Developed by Zaid Neurothrone"),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Weather App"),
       ),
+      body: const AboutContent(),
     );
   }
 }
