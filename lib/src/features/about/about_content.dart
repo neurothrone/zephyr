a968b3327d4e988435872d52999e7ab351728190
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-04 06:50:21 +0200

    improve layout

--- lib/src/features/about/about_content.dart
+++ lib/src/features/about/about_content.dart
@@ -9,35 +9,43 @@ class AboutContent extends StatelessWidget {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
 
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(kPadding20),
-      child: SizedBox(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              "Project Weather",
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
+    return LayoutBuilder(
+        builder: (context, BoxConstraints viewportConstraints) {
+      return SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: kPadding20),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: viewportConstraints.maxHeight,
+            minWidth: viewportConstraints.maxWidth,
+          ),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "Project Weather",
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: kPadding20),
+                SizedBox(
+                  width: mediaQuery.orientation == Orientation.portrait
+                      ? mediaQuery.size.width
+                      : mediaQuery.size.width / 2,
+                  child: const Text(
+                    "This is an app that is developed for the course 1DV535 at "
+                    "Linneaus University using Flutter and the OpenWeatherMap API.",
+                  ),
+                ),
+                const SizedBox(height: kPadding10),
+                const Text("Developed by Zaid Neurothrone"),
+              ],
             ),
-            const SizedBox(height: kPadding20),
-            SizedBox(
-              width: mediaQuery.orientation == Orientation.portrait
-                  ? mediaQuery.size.width
-                  : mediaQuery.size.width / 2,
-              child: const Text(
-                "This is an app that is developed for the course 1DV535 at "
-                "Linneaus University using Flutter and the OpenWeatherMap API.",
-              ),
-            ),
-            const SizedBox(height: kPadding10),
-            const Text("Developed by Zaid Neurothrone"),
-          ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
