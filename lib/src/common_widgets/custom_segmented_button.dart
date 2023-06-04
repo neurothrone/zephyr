bff63223a3d385fbc1aa574ff892c8fd96aa2036
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-04 06:27:04 +0200

    update import

--- lib/src/common_widgets/custom_segmented_button.dart
+++ lib/src/common_widgets/custom_segmented_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../features/current_weather/domain/location_weather_type.dart';
+import '../features/current_weather/presentation/current_weather_page_tab_bar.dart';
 
 class CustomSegmentedButton extends StatefulWidget {
   const CustomSegmentedButton({
