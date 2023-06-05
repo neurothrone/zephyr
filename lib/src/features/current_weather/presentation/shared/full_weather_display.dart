17c25a9d5399b4e8c8a587f1da345dceeacc7824
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-05 10:07:11 +0200

    remove conditional

--- lib/src/features/current_weather/presentation/shared/full_weather_display.dart
+++ lib/src/features/current_weather/presentation/shared/full_weather_display.dart
@@ -17,16 +17,12 @@ class FullWeatherDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         CurrentWeatherDisplay(weather: weather),
-        // This fixes the spacing that is lost in landscape mode
-        if (mediaQuery.orientation == Orientation.landscape)
-          SizedBox(height: mediaQuery.size.height * 0.1),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.1),
         LocationForecastWeatherDisplay(
           forecastList: forecastList,
         ),
