350bd24b926635af18c59a2b3d079e2bf9e2e4c9
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-06 03:49:01 +0200

    extract out Column

--- lib/src/features/about/about_content.dart
+++ lib/src/features/about/about_content.dart
@@ -7,8 +7,6 @@ class AboutContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-
     return LayoutBuilder(
         builder: (context, BoxConstraints viewportConstraints) {
       return SingleChildScrollView(
@@ -18,34 +16,47 @@ class AboutContent extends StatelessWidget {
             minHeight: viewportConstraints.maxHeight,
             minWidth: viewportConstraints.maxWidth,
           ),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  "Project Weather",
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: kPadding20),
-                SizedBox(
-                  width: mediaQuery.orientation == Orientation.portrait
-                      ? mediaQuery.size.width
-                      : mediaQuery.size.width / 2,
-                  child: const Text(
-                    "This is an app that is developed for the course 1DV535 at "
-                    "Linneaus University using Flutter and the OpenWeatherMap API.",
-                  ),
-                ),
-                const SizedBox(height: kPadding10),
-                const Text("Developed by Zaid Neurothrone"),
-              ],
-            ),
+          child: const Center(
+            child: AboutContentColumn(),
           ),
         ),
       );
     });
+  }
+}
+
+class AboutContentColumn extends StatelessWidget {
+  const AboutContentColumn({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Text(
+          "Project Weather",
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: kPadding20),
+        SizedBox(
+          width: mediaQuery.orientation == Orientation.portrait
+              ? mediaQuery.size.width
+              : mediaQuery.size.width / 2,
+          child: const Text(
+            "This is an app that is developed for the course 1DV535 at "
+            "Linneaus University using Flutter and the OpenWeatherMap API.",
+          ),
+        ),
+        const SizedBox(height: kPadding10),
+        const Text("Developed by Zaid Neurothrone"),
+      ],
+    );
   }
 }
